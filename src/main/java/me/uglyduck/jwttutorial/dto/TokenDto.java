40eb2830c2c67ec2cd4e5d2f25495947ad319180
YongJun Lee <mlionlove54@gmail.com>
2021-03-11 21:38:35 +0900

    DTO, Repository, 로그인
    - 외부와의 통신에 사용할 DTO 클래스 생성
    - Repository 관련 코드 생성
    - 로그인 API, 관련 로직 생성

---
+++ src/main/java/me/uglyduck/jwttutorial/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package me.uglyduck.jwttutorial.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
